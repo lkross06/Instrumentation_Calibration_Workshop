01d2ab82e0c68168336a96ca077370e1b07f4fed
lkross06 <lucaskalaniross@gmail.com>
2025-11-07 12:34:14 -0800

    feat: added setup code

--- Load Cell Calibration/src/main.cpp
+++ Load Cell Calibration/src/main.cpp
@@ -15,13 +15,30 @@
 #define SYNC -1
 
 //SPI bus
-SPIClass spi(FSPI);     //The ESP32 has multiple SPI buses, and FSPI is the SPI3 bus
+SPIClass spi_bus(FSPI);     //The ESP32 has multiple SPI buses, and FSPI is the SPI3 bus
 
 //ADC instance
-ADS1256 adc(DRDY, RESET, SYNC, CS, VREF, &spi);
+ADS1256 adc(DRDY, RESET, SYNC, CS, VREF, &spi_bus);
 
 void setup() {
-    
+    Serial.begin(115200);
+    delay(1000);
+
+    //configure SPI pins
+    pinMode(MISO, INPUT_PULLUP);
+    pinMode(SCLK, OUTPUT);
+    pinMode(MOSI, OUTPUT);
+
+    //initialize the SPI bus with custom pins
+    spi_bus.begin(SCLK, MISO, MOSI, CS);
+
+    //ADC Setup
+    adc.InitializeADC();            //setup spi interface
+    adc.setPGA(PGA_64);             //improve resolution for small signals by 64x
+    adc.setMUX(DIFF_0_1);           //ready differentially (compare analog input 0 to analog input 1)
+    adc.setDRATE(DRATE_1000SPS);    //set data rate to 1k sps
+
+    delay(1000);
 }
 
 void loop() {
