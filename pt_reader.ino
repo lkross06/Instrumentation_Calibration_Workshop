3c6d0cae84d1eb5ef8ab7cdb010e3fcde55dad60
lkross06 <lucaskalaniross@gmail.com>
2025-11-09 20:26:21 -0800

    feat: refactored /pt_reader.ino for prometheus 24-25 DAQ

--- pt_reader.ino
+++ pt_reader.ino
@@ -1,29 +1,55 @@
 #include <Arduino.h>
-#include <ADS8688.h>
+#include "ADS8688.h"
+#include <SPI.h>
 
 //SPI pins
-#define MISO 37
-#define SCLK 36
-#define MOSI 35
-#define CS1 38
-#define CS2 39
+#define MISO 35
+#define SCLK 48
+#define MOSI 34
 
-//other ADC settings
+//ADC (ADS8688) settings
+#define ADS8688_CS 36
 #define VREF 4.1
 #define RANGE 0x05
 
-ADS8688 adc; //create ADC instance
+//LED indicator pin (see below)
+#define LED 38
+
+//SPI bus connecting between ESP32 and ADC
+SPIClass spi(FSPI);   //The ESP32 has multiple SPI buses. FSPI sets this to the SPI3 bus
+
+//ADS8688 interface
+ADS8688 pressureADC;
 
 void setup() {
-    Serial.begin(115200);
-    delay(1000);
+  Serial.begin(115200);   //baud rate = 115200 baud
+  delay(1000);
 
-    //initialize ADC
-    adc.begin(MISO, SCLK, MOSI, CS1, CS2, VREF, RANGE);
-    adc.setInputRange(CS1, RANGE);
-    adc.setInputRange(CS2, RANGE);
+  
+  //This pin controls an orange LED in the middle of the DAQ. We will
+  //  turn on the LED when our program starts, so we know that our
+  //  code was flashed + executed successfully.
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, HIGH);
+
+  //Start SPI bus
+  spi.begin(SCLK, MISO, MOSI, ADS8688_CS);
+
+  //Initialize ADC
+  pressureADC.begin(MISO, SCLK, MOSI, ADS8688_CS, 7, VREF, RANGE);    //this library connects to 2 ADCs, and this board has the second ADC's CS on pin 7
+  pressureADC.setInputRange(ADS8688_CS, 0x05);    //don't worry about this setting too much for now
+
+  Serial.println("Initialization complete.");
 }
 
 void loop() {
-    
+  //The PT ADC has 8 channels to read PTs from. We only have 1 PT connected rn,
+  //  so we only will print one of these values (the others are floating-- nonsense noise)
+  float ptVoltages[8];
+  
+  pressureADC.readAllChannels(ADS8688_CS, true, ptVoltages);
+
+  Serial.println(ptVoltages[7]);    //Ryan hopefull will hook up the PT to PT0 on the DAQ...
+
+  delay(200);   //wait 200ms before reading again
 }
